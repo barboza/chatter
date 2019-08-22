2236cda346845949a388098885b82d593728723f
Rafael Barboza <barboza.cardoso@gmail.com>
2019-08-22 12:57:33 -0300

    Add Phoenix base app and Live View configuration
    Creates a new umbrella Phoenix Application and configures it to use
    Phoenix Live View (according to the repo instructions)

---
+++ README.md
@@ -0,0 +1,3 @@
+# Chatter.Umbrella
+
+Simple chat service using Elixir, Phoenix and Phoenix Live View.
---
+++ apps/chatter_web/lib/chatter_web/application.ex
@@ -0,0 +1,29 @@
+defmodule ChatterWeb.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    # List all child processes to be supervised
+    children = [
+      # Start the endpoint when the application starts
+      ChatterWeb.Endpoint
+      # Starts a worker by calling: ChatterWeb.Worker.start_link(arg)
+      # {ChatterWeb.Worker, arg},
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: ChatterWeb.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+  def config_change(changed, _new, removed) do
+    ChatterWeb.Endpoint.config_change(changed, removed)
+    :ok
+  end
+end
